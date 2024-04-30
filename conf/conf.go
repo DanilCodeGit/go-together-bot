95eafa5479b64e0694829dc799d88c8bbf5bdcfb
danil <naumkin-02@mail.ru>
2024-04-30 20:46:42 +0300

    fix layout

--- conf/conf.go
+++
@@ -1,6 +0,0 @@
-package conf
-
-const TelegramBotApiKey = "6824843543:AAGsyTi97soRJFZsJJ5UKBhFlKwrCkT6PGs"
-
-const DSN = "cr50181_ridebot:root@tcp(vh326.timeweb.ru:3306)/cr50181_ridebot"
-const DSNLocal = "root:dixedDIX-111@tcp(localhost:3306)/ride_bot"
